745eb19de8294dd8014aeae30bcebef11282627c
bmolzea <bomolzea722@g.educaand.es>
2025-02-07 17:46:34 +0100

    Ejercicios UD4

---
+++ Primero/Programación/UD4/ejercicio1.py
@@ -0,0 +1,5 @@
+andalucia = ["Almería", "Cádiz", "Granada", "Málaga", "Jaén", "Córdoba", "Huelva", "Sevilla"]
+
+for provincia in andalucia:
+    if provincia[0] == "C":
+        print(provincia)
---
+++ Primero/Programación/UD4/pruebas_listas.py
@@ -0,0 +1,15 @@
+frutas = ["Pera", "Manzana", "Plátano", "Fresa"]
+jugadores = ["Iniesta", "Iker Casillas", "Xavi"]
+elems = [-2323, "Ax5", True, "tri"]
+
+
+for fruta in frutas:
+   print(fruta)
+
+
+for x in jugadores:
+   print(x)
+
+
+for _ in elems:
+   print(_)
